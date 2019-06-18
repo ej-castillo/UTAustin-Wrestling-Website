7ac32efa4ae4ff91df7093564ec9b8d1eca7b446
root <root@EJ-XPS-13.localdomain>
2019-06-18 12:04:33 -0500

    added files

--- flask/main.py
+++ flask/main.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
-def create_app(test_config=None)
-	app = Flask(__name__)
+app = Flask(__name__)
 
-	@app.route("/"):
-	def home():
-		return 'Hello, world!'
+@app.route("/")
+def index():
+	return render_template("index.html")
 
-if __name__ == "__main__":
-	app,run(debug=True)
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
+@app.route("/schedule")
+def schedule():
+	return render_template("schedule.html")
+
+# if __name__ == "__main__":
+# 	app.run(debug=True)
